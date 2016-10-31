751974c1fa296eb08ce7ba8d2936306fa03ca2b7
john <john.b.y.yang@gmail.com>
2016-10-31 10:05:55 +0800

    ....

--- midser/models.py
+++ midser/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class vgitem(models.Model):
+    country = models.CharField(max_length=20)
+    ipadd = models.CharField(max_length=20)
+    ovpnfile = models.CharField(max_length=5000)
+    pub_date = models.DateTimeField('date published')
+
